d46ad5f0583b5dad76898a3e794264d0ab575cdd
MuhammadTaufiqAbdusSalam <arekmoker65@gmail.com>
2024-10-08 15:43:47 +0700

    upload js 6

---
+++ Pertemuan 6/layout_flutter/lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+// Praktikum 1
+ class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter layout: Muhammad taufiq Abdus Salam/2241760040',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/Pemandangan.jpg',
+              width: 600,
+              height:240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,  // Memanggil widget titleSection yang sudah kita buat
+            buttonSection, // Adding the button section
+            textSection
+          ],
+        ),
+      ),
+    );
+  }
+  Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: const Text(
+    'Carilah teks di internet yang sesuai '
+    'dengan foto atau tempat wisata yang ingin '
+    'Anda tampilkan. '
+    'Muhammad Taufiq Abdus Salam/2241760040 '
+    'identitas hasil pekerjaan Anda. '
+    'Selamat mengerjakan 🙂.',
+    softWrap: true,
+  ),
+);
+// Widget titleSection yang telah kita definisikan
+Widget titleSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+        //soal 1
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //soal 2
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: const Text(
+                'Wisata Gunung di Batu',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Text(
+              'Batu, Malang, Indonesia',
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+      //soal 3
+      Icon(
+        Icons.star,
+        color: Colors.red,
+      ),
+      const Text('41'),
+    ],
+  ),
+);
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
